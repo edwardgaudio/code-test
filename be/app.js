a6f8a3238916faaf4e9ebe7394bc17a206b8a01c
Tyler Fishbone <tfishbone@zulily.com>
2023-04-27 16:43:11 -0700

    add cors

--- be/app.js
+++ be/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var productsRouter = require('./routes/products');
@@ -13,6 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }))
 
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
